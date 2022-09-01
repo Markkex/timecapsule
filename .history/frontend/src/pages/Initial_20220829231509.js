7668ca9e369e74b607cb12c40427fb70f271da66
markkex <avila.marco.dev@gmail.com>
2022-09-01 17:54:31 +0100

    updates to git ignore

---
+++ .history/frontend/src/pages/Initial_20220829231509.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Initial = () => {
+  return (
+    <div className="initial">
+      <video
+        autoPlay={true}
+        loop={true}
+        controls={false}
+        playsInline
+        muted
+        id="myVideo"
+      >
+        <source type="video/mp4" />
+        Your browser does not support HTML5 video.
+      </video>
+    </div>
+  );
+};
+
+export default Initial;
